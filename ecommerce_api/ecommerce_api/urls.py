4996bce661be63668db4cb27f5166bcaf11ee953
Matheus Lima <matheuslimacarneirors@gmail.com>
2025-12-11 01:40:08 -0300

    feat(customers): adiciona testes de model/serializer/views e ajusta AppConfig e urls para apps.customers

--- ecommerce_api/ecommerce_api/urls.py
+++ ecommerce_api/ecommerce_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/customers/', include('customers.urls', namespace='customers')),
+    path('api/customers/', include('apps.customers.urls', namespace='customers')),
     path('api/categories/', include('apps.categories.urls', namespace='categories')),
 ]
